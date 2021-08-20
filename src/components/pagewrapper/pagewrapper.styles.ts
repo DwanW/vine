b2a2b91f63f4c7818af616f1852f8b9a4860b2b8
DwanW <kunvang@yahoo.com>
2021-08-19 18:18:42 -0600

    fix: fixing overflow wrapper

--- src/components/pagewrapper/pagewrapper.styles.ts
+++ src/components/pagewrapper/pagewrapper.styles.ts
@@ -3,8 +3,10 @@ import styled from "styled-components";
 export const WrapperMain = styled.main`
   width: 100%;
   margin: auto;
+  padding: 0.5rem;
 
   @media (min-width: 640px) {
     margin-left: 145px;
+    width: auto;
   }
 `;
