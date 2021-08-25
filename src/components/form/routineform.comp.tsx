d0f06633e005f746cce80ef96d8cc12a3a73bba4
DwanW <kunvang@yahoo.com>
2021-08-24 21:47:33 -0600

    feat: basic step3 form functionality complete

--- src/components/form/routineform.comp.tsx
+++ src/components/form/routineform.comp.tsx
@@ -1,10 +1,12 @@
 import Stepper from "@material-ui/core/Stepper";
 import Step from "@material-ui/core/Step";
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import { AltStepLabel } from "./routineform.styles";
 import TextField from "@material-ui/core/TextField";
 import MenuItem from "@material-ui/core/MenuItem";
 import Radio from "@material-ui/core/Radio";
+import { Checkbox } from "@material-ui/core";
+import dayjs from "dayjs";
 
 const steps = [
   "Select Type of Progress",
@@ -70,6 +72,8 @@ const RoutineForm = ({ closeForm }: Props) => {
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     let updatedRoutine: { [key: string]: any } = { ...routine };
     updatedRoutine[e.target.name] = e.target.value;
+
+    console.log(updatedRoutine);
     setRoutine(updatedRoutine as RoutineObj);
   };
 
@@ -80,7 +84,6 @@ const RoutineForm = ({ closeForm }: Props) => {
     } else {
       updatedRoutine.goal = `${e.target.value}${updatedRoutine.goal.slice(1)}`;
     }
-    console.log(updatedRoutine.goal);
     setRoutine(updatedRoutine as RoutineObj);
   };
 
@@ -92,7 +95,47 @@ const RoutineForm = ({ closeForm }: Props) => {
     } else {
       updatedRoutine.goal = `${updatedRoutine.goal[0]}${e.target.value}`;
     }
-    console.log(updatedRoutine.goal);
+    setRoutine(updatedRoutine as RoutineObj);
+  };
+
+  const handleCheckBoxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let updatedRoutine: { [key: string]: any } = { ...routine };
+    if (!e.target.checked) {
+      updatedRoutine.schedule = updatedRoutine.schedule
+        .split("")
+        .filter((value: string) => value !== e.target.value)
+        .join("");
+    } else {
+      updatedRoutine.schedule = `${updatedRoutine.schedule}${e.target.value}`;
+    }
+    setRoutine(updatedRoutine as RoutineObj);
+  };
+
+  const handlePerPeriodChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (parseInt(e.target.value) < 1 || isNaN(parseInt(e.target.value))) return;
+    let updatedRoutine: { [key: string]: any } = { ...routine };
+    updatedRoutine.schedule = `${e.target.value}${
+      updatedRoutine.schedule[updatedRoutine.schedule.length - 1]
+    }`;
+    setRoutine(updatedRoutine as RoutineObj);
+  };
+
+  const handleSelectPerPeriod = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let updatedRoutine: { [key: string]: any } = { ...routine };
+    if (updatedRoutine.schedule.includes(e.target.value)) {
+      return;
+    }
+    updatedRoutine.schedule = `${updatedRoutine.schedule.slice(
+      0,
+      updatedRoutine.schedule.length - 1
+    )}${e.target.value}`;
+    setRoutine(updatedRoutine as RoutineObj);
+  };
+
+  const handleRepeat = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (parseInt(e.target.value) < 2 || isNaN(parseInt(e.target.value))) return;
+    let updatedRoutine: { [key: string]: any } = { ...routine };
+    updatedRoutine.schedule = `e${e.target.value}`;
     setRoutine(updatedRoutine as RoutineObj);
   };
 
@@ -192,46 +235,109 @@ const RoutineForm = ({ closeForm }: Props) => {
               checked={routine.schedule === "1234567"}
               onChange={handleChange}
               value="1234567"
-              name="step3"
+              name="schedule"
               inputProps={{ "aria-label": "everyday" }}
             />
             <span>EveryDay</span>
           </div>
           <div>
-            <Radio
-              checked={
-                routine.schedule.length > 0 && routine.schedule[0] === "s"
-              }
-              onChange={handleChange}
-              value="s"
-              name="step3"
-              inputProps={{ "aria-label": "some days of the week" }}
-            />
-            <span>Some days of the week</span>
+            <div>
+              <Radio
+                checked={
+                  routine.schedule.length > 0 && routine.schedule[0] === "s"
+                }
+                onChange={handleChange}
+                value="s"
+                name="schedule"
+                inputProps={{ "aria-label": "some days of the week" }}
+              />
+              <span>Some days of the week</span>
+            </div>
+            {routine.schedule.length > 0 && routine.schedule[0] === "s" && (
+              <div>
+                {"1234567".split("").map((num) => (
+                  <Fragment key={num}>
+                    <Checkbox
+                      checked={routine.schedule.includes(num)}
+                      value={num}
+                      onChange={handleCheckBoxChange}
+                      inputProps={{ "aria-label": `day checkboxes ${num}` }}
+                    />
+                    <span>
+                      {dayjs()
+                        .day(parseInt(num) - 1)
+                        .format("dddd")}
+                    </span>
+                  </Fragment>
+                ))}
+              </div>
+            )}
           </div>
           <div>
-            <Radio
-              checked={
-                routine.schedule.length > 0 && /[mn]/.test(routine.schedule)
-              }
-              onChange={handleChange}
-              value="1w"
-              name="step3"
-              inputProps={{ "aria-label": "some times per period" }}
-            />
-            <span>Some times per period</span>
+            <div>
+              <Radio
+                checked={
+                  routine.schedule.length > 0 && /[mw]/.test(routine.schedule)
+                }
+                onChange={handleChange}
+                value="1w"
+                name="schedule"
+                inputProps={{ "aria-label": "some times per period" }}
+              />
+              <span>Some times per period</span>
+            </div>
+            {routine.schedule.length > 0 && /[mw]/.test(routine.schedule) && (
+              <div>
+                <TextField
+                  variant="filled"
+                  name="schedule"
+                  value={parseInt(
+                    routine.schedule.slice(0, routine.schedule.length - 1)
+                  )}
+                  onChange={handlePerPeriodChange}
+                  inputProps={{ inputMode: "numeric" }}
+                  autoComplete="off"
+                />
+                times per
+                <TextField
+                  select
+                  value={routine.schedule[routine.schedule.length - 1]}
+                  onChange={handleSelectPerPeriod}
+                  variant="outlined"
+                >
+                  <MenuItem value={"w"}>Week</MenuItem>
+                  <MenuItem value={"m"}>Month</MenuItem>
+                </TextField>
+              </div>
+            )}
           </div>
           <div>
-            <Radio
-              checked={
-                routine.schedule.length > 0 && routine.schedule[0] === "e"
-              }
-              onChange={handleChange}
-              value="e2"
-              name="step3"
-              inputProps={{ "aria-label": "repeat over a period" }}
-            />
-            <span>Repeat</span>
+            <div>
+              <Radio
+                checked={
+                  routine.schedule.length > 0 && routine.schedule[0] === "e"
+                }
+                onChange={handleChange}
+                value="e2"
+                name="schedule"
+                inputProps={{ "aria-label": "repeat over a period" }}
+              />
+              <span>Repeat</span>
+            </div>
+            {routine.schedule.length > 0 && routine.schedule[0] === "e" && (
+              <div>
+                every{" "}
+                <TextField
+                  variant="filled"
+                  name="schedule"
+                  value={parseInt(routine.schedule.slice(1))}
+                  onChange={handleRepeat}
+                  inputProps={{ inputMode: "numeric" }}
+                  autoComplete="off"
+                />{" "}
+                days
+              </div>
+            )}
           </div>
         </div>
       )}
