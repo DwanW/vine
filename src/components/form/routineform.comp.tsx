bb35ee681eb6d2cbf0973a2d2b039ca1f6cb143a
DwanW <kunvang@yahoo.com>
2021-08-24 18:10:59 -0600

    feat: added routine object definition

--- src/components/form/routineform.comp.tsx
+++ src/components/form/routineform.comp.tsx
@@ -14,8 +14,34 @@ interface Props {
   closeForm: Function;
 }
 
+export interface RecordObj {
+  date: Date;
+  value: boolean | number;
+}
+
+export interface RoutineObj {
+  name: string;
+  goal?: string; //'<3' less than three a day, '=3' exactly 3, '>3' at least 3.
+  unit?: string;
+  schedule: string; // everyday is '1234567', somedays of a week is eg: weekends'17', times per period is eg: 3times per week '3w', per month '3m', repeat eg: every 10 days is 'e10'
+  startdate: Date | undefined;
+  enddate: Date | undefined;
+  reminders: Date[];
+  priority: number;
+  record: RecordObj[];
+}
+
 const RoutineForm = ({ closeForm }: Props) => {
   const [activeStep, setActiveStep] = useState(0);
+  const [routine, setRoutine] = useState<RoutineObj>({
+    name: "",
+    schedule: "",
+    startdate: undefined,
+    enddate: undefined,
+    reminders: [],
+    priority: 1,
+    record: [],
+  });
 
   const handleSubmit = (e: any) => {
     e.preventDefault();
@@ -25,7 +51,6 @@ const RoutineForm = ({ closeForm }: Props) => {
 
   return (
     <form onSubmit={handleSubmit}>
-      this is routine form
       <Stepper activeStep={activeStep} alternativeLabel>
         {steps.map((label) => (
           <Step key={label}>
@@ -33,20 +58,30 @@ const RoutineForm = ({ closeForm }: Props) => {
           </Step>
         ))}
       </Stepper>
-      <button
-        type="button"
-        onClick={() => setActiveStep(activeStep - 1)}
-        disabled={activeStep === 0}
-      >
-        back
-      </button>
-      {activeStep === steps.length - 1 ? (
-        <input type="submit" value="save" />
-      ) : (
-        <button type="button" onClick={() => setActiveStep(activeStep + 1)}>
-          next
-        </button>
+
+      {activeStep === 0 && (
+        <div>
+          <button>With A Yes Or No</button>
+          <button>With A Numeric Value</button>
+        </div>
       )}
+
+      <div>
+        <button
+          type="button"
+          onClick={() => setActiveStep(activeStep - 1)}
+          disabled={activeStep === 0}
+        >
+          back
+        </button>
+        {activeStep === steps.length - 1 ? (
+          <input type="submit" value="save" />
+        ) : (
+          <button type="button" onClick={() => setActiveStep(activeStep + 1)}>
+            next
+          </button>
+        )}
+      </div>
     </form>
   );
 };
