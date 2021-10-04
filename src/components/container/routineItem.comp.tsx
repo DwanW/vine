2bae032d6a2065b8187ad2ecc8c58498cea4ffff
DwanW <kunvang@yahoo.com>
2021-10-03 23:15:08 -0600

    feat: added menu buttons

--- src/components/container/routineItem.comp.tsx
+++ src/components/container/routineItem.comp.tsx
@@ -1,9 +1,12 @@
+import { Menu, MenuItem } from "@mui/material";
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import {
   calculateCompletion,
   calculateStreak,
   scheduleToString,
 } from "../../util/validation";
+import { IconButton } from "./common.styles";
 import { RoutineItemContainer } from "./routineItem.styles";
 
 interface Props {
@@ -11,13 +14,23 @@ interface Props {
 }
 
 const RoutineItem = ({ routine }: Props) => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <RoutineItemContainer>
       <div>{routine.name}</div>
       <div>{scheduleToString(routine.schedule)}</div>
       <div>
         <span>
-          streak:{" "}
+          Highest Streak:{" "}
           {
             calculateStreak(
               routine.records,
@@ -27,7 +40,7 @@ const RoutineItem = ({ routine }: Props) => {
           }
         </span>{" "}
         <span>
-          completion:{" "}
+          Completion:{" "}
           {calculateCompletion(
             routine.records,
             routine.schedule,
@@ -35,9 +48,45 @@ const RoutineItem = ({ routine }: Props) => {
           ) * 100}{" "}
           %
         </span>{" "}
-        <NavLink to={`/light/${routine.id}`}>light graph</NavLink>
-        <NavLink to={`/stats/${routine.id}`}>statistics</NavLink>
-        <span>menu button (delete) (edit)</span>
+        <div>
+          <NavLink to={`/light/${routine.id}`}>
+            <img src="assets/icon/light.svg" alt="lightgraph button" />
+          </NavLink>
+          <NavLink to={`/stats/${routine.id}`}>
+            <img src="assets/icon/stats.svg" alt="stats button" />
+          </NavLink>
+          <IconButton onClick={handleClick}>
+            <img src="assets/icon/vert.svg" alt="stats button" />
+          </IconButton>
+        </div>
+        <Menu
+          id="menu"
+          aria-labelledby="menu-list"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: "top",
+            horizontal: "left",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "left",
+          }}
+        >
+          <MenuItem onClick={handleClose}>
+            <IconButton>
+              <img src="assets/icon/edit.svg" alt="edit button" />
+              <span>Edit</span>
+            </IconButton>
+          </MenuItem>
+          <MenuItem onClick={handleClose}>
+            <IconButton>
+              <img src="assets/icon/delete.svg" alt="delete button" />
+              <span>Delete</span>
+            </IconButton>
+          </MenuItem>
+        </Menu>
       </div>
     </RoutineItemContainer>
   );
